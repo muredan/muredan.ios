7cea6164e1ab69a97c84fd9dd9d77d6f01b270db
Muresan Dan <44547003+muredan@users.noreply.github.com>
2022-11-02 12:38:43 +0200

    update

--- .github/scripts/archive_app.sh
+++ .github/scripts/archive_app.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 set -eo pipefail
+# create variables
+WORKSPACE=muredan.ios.xcworkspace
+SCHEME=muredan.ios
+ARCHIVE_PATH=$PWD/build/muredan.ios.xcarchive
 
-xcodebuild -workspace muredan.ios.xcworkspace \
-            -scheme muredan.ios\ iOS \
+xcodebuild -workspace $WORKSPACE \
+            -scheme $SCHEME \
             -sdk iphoneos \
             -configuration AppStoreDistribution \
-            -archivePath $PWD/build/muredan.ios.xcarchive \
+            -archivePath $ARCHIVE_PATH \
             clean archive | xcpretty
