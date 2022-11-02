518aabbf9e4b1ee98425a63dc64c8d2742179d93
Muresan Dan <44547003+muredan@users.noreply.github.com>
2022-11-02 23:06:51 +0200

    update

--- .github/scripts/archive_app.sh
+++ .github/scripts/archive_app.sh
@@ -12,6 +12,6 @@ xcodebuild -workspace $WORKSPACE \
             -allowProvisioningUpdates \
             -scheme $SCHEME \
             -sdk iphoneos \
+            -configuration AppStoreDistribution \
             # -derivedDataPath build/DerivedData
-            # -configuration AppStoreDistribution \
             # -archivePath $ARCHIVE_PATH archive
