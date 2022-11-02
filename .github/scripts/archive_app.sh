6f5a42f5bf7557f650e7b80483f611a49ac11c66
Muresan Dan <44547003+muredan@users.noreply.github.com>
2022-11-02 16:21:37 +0200

    update

--- .github/scripts/archive_app.sh
+++ .github/scripts/archive_app.sh
@@ -11,5 +11,6 @@ xcodebuild -workspace $WORKSPACE \
             -scheme $SCHEME \
             -sdk iphoneos \
             -configuration Release \
+            -allowProvisioningUpdates \
             -archivePath $PWD/build/muredan.ios.xcarchive \
             clean archive | xcpretty
