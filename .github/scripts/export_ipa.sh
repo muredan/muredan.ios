4d69b86292314621995725fbe2a6a5440b386cec
Muresan Dan <44547003+muredan@users.noreply.github.com>
2022-11-02 15:24:39 +0200

    update

--- .github/scripts/export_ipa.sh
+++ .github/scripts/export_ipa.sh
@@ -3,6 +3,7 @@
 set -eo pipefail
 
 xcodebuild -archivePath $PWD/build/muredan.ios.xcarchive \
+            -exportOptionsPlist muredan.ios/exportOptions.plist \
             -exportPath $PWD/build \
             -allowProvisioningUpdates \
             -exportArchive | xcpretty
