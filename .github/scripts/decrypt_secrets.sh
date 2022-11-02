1196a1aaa4ea9ca3d6a7ca3f089a1227b85f00ef
Muresan Dan <44547003+muredan@users.noreply.github.com>
2022-11-02 11:24:16 +0200

    update

--- .github/scripts/decrypt_secrets.sh
+++ .github/scripts/decrypt_secrets.sh
@@ -4,7 +4,7 @@ set -eo pipefail
 # create variables
 CERTIFICATE_PATH=$RUNNER_TEMP/build_certificate.p12
 PP_PATH=$RUNNER_TEMP/build_pp.mobileprovision
-KEYCHAIN_PATH=$RUNNER_TEMP/app-signing.keychain-db
+KEYCHAIN_PATH=$RUNNER_TEMP/build.keychain
 
 # import certificate and provisioning profile from secrets
 echo -n "$BUILD_CERTIFICATE_BASE64" | base64 --decode -o $CERTIFICATE_PATH
@@ -16,7 +16,7 @@ security set-keychain-settings -lut 21600 $KEYCHAIN_PATH
 security unlock-keychain -p "$KEYCHAIN_PASSWORD" $KEYCHAIN_PATH
 
 # import certificate to keychain
-security import $CERTIFICATE_PATH -P "$P12_PASSWORD" -A -t cert -f pkcs12 -k $KEYCHAIN_PATH
+security import $CERTIFICATE_PATH -t agg -k $KEYCHAIN_PATH -P "$P12_PASSWORD" -A
 security list-keychain -d user -s $KEYCHAIN_PATH
 
 # apply provisioning profile
