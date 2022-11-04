2aaee6e557d3d8f22e4eaaf678c0d474935f319e
Muresan Dan <44547003+muredan@users.noreply.github.com>
2022-11-04 16:52:49 +0200

    update

--- .github/scripts/archive_app.sh
+++ .github/scripts/archive_app.sh
@@ -11,7 +11,7 @@ ARCHIVE_PATH=build/muredan.ios
 xcodebuild -workspace $WORKSPACE \
             -scheme $SCHEME \
             -sdk iphoneos \
-            -destination "generic/platform=iOS"
+            -destination "generic/platform=iOS" \
             -configuration Release build CODE_SIGNING_ALLOWED=NO
             # -archivePath $ARCHIVE_PATH archive \
             # -allowProvisioningUpdates \
